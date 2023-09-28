861a6adcb319cddb887fb62183cc4bc397c26446
Serhii Zashchelkin <serg.zz@ukr.net>
2023-09-28 19:28:33 +0200

    Updates

--- pman_lib/src/pman/crypto.rs
+++ pman_lib/src/pman/crypto.rs
@@ -2,3 +2,11 @@ pub trait CryptoProcessor {
     fn encode(&self, data: [u8;64]) -> [u8;64];
     fn decode(&self, data: &[u8;64]) -> [u8;64];
 }
+
+pub struct AesProcessor {
+
+}
+
+impl CryptoProcessor for AesProcessor {
+
+}
